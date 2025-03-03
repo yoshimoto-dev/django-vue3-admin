a4e97cc93228f78d8511ea870bad48e52cc08e70
Coly Cao <colycao.dev@gmail.com>
2025-03-04 00:19:59 +0900

    Fix for docker.

--- backend/application/settings.py
+++ backend/application/settings.py
@@ -186,7 +186,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": ["redis://:DVADMIN3@dvadmin3-redis:6379/0"],
+            "hosts": ["redis://:DVADMIN3@redis:6379/0"],
             # "hosts": [('127.0.0.1', 6379)], #需修改
         },
     },
--- init.sh
+++ init.sh
@@ -15,17 +15,17 @@ else
     echo "REDIS随机密码已生成并写入 $ENV_FILE 文件。"
     
     awk 'BEGIN { cmd="cp -i ./backend/conf/env.example.py   ./backend/conf/env.py "; print "n" |cmd; }'
-    sed -i "s|DATABASE_HOST = 'dvadmin3-mysql'|DATABASE_HOST = '177.10.0.13'|g" ./backend/conf/env.py
-    sed -i "s|REDIS_HOST = 'dvadmin3-redis'|REDIS_HOST = '177.10.0.15'|g" ./backend/conf/env.py
+    sed -i "s|DATABASE_HOST = 'mysql'|DATABASE_HOST = '177.10.0.13'|g" ./backend/conf/env.py
+    sed -i "s|REDIS_HOST = 'redis'|REDIS_HOST = '177.10.0.15'|g" ./backend/conf/env.py
     sed -i "s|DATABASE_PASSWORD = 'DVADMIN3'|DATABASE_PASSWORD = '$MYSQL_PASSWORD'|g" ./backend/conf/env.py
     sed -i "s|REDIS_PASSWORD = 'DVADMIN3'|REDIS_PASSWORD = '$REDIS_PASSWORD'|g" ./backend/conf/env.py
     echo "初始化密码创建成功"
 fi
 
 docker-compose up -d
-docker exec dvadmin3-django python manage.py makemigrations
-docker exec dvadmin3-django python manage.py migrate
-docker exec dvadmin3-django python manage.py init
+docker exec django python manage.py makemigrations
+docker exec django python manage.py migrate
+docker exec django python manage.py init
 echo "欢迎使用dvadmin3项目"
 echo "登录地址：http://ip:8080"
 echo "如访问不到，请检查防火墙配置"
