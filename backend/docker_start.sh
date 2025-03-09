f2bd1f350ce4cd819325b2784980d0f5e0c24b52
Coly Cao <colycao.dev@gmail.com>
2025-03-09 09:40:26 +0900

    Fix for django reload.

--- backend/docker_start.sh
+++ backend/docker_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# python manage.py makemigrations
-# python manage.py migrate
-# python manage.py init -y
+python manage.py makemigrations
+python manage.py migrate
+python manage.py init_area
+python manage.py init -y
 uvicorn application.asgi:application --port 8001 --host 0.0.0.0 --workers 4
